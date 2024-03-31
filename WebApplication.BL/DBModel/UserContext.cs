9ffae9c6cdb3868e983e224376c5cf0c5e89fac9
EvaIablocova <125370412+EvaIablocova@users.noreply.github.com>
2024-03-31 07:16:31 +0200

    add Db

--- WebApplication.BL/DBModel/UserContext.cs
+++ WebApplication.BL/DBModel/UserContext.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApplication.Domain.Entities.User;
 
 namespace WebApplication.BL.DBModel
 {
-    public class UserContext
+    public class UserContext : DbContext
     {
+        public UserContext()
+        : base ("WebApp") 
+        { }
+
+        public virtual DbSet<UDbTable> Users { get; set; }
     }
 }
