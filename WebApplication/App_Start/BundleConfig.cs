32b0cf48eefa6f4991e56ec77208060dc07a7507
IvanovaArina <147144869+IvanovaArina@users.noreply.github.com>
2024-05-21 16:48:07 +0300

    bundleees

---
+++ WebApplication/App_Start/BundleConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+           
+            // Template style
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/assets/css").Include(
+            "~/assets/css/vendor/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/assets/css").Include(
+            "~/assets/css/vendor/font-awesome.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/slick-theme/css").Include(
+            "~/assets/css/vendor/slick-theme.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/aksVideoPlayer/css").Include(
+            "~/assets/css/vendor/aksVideoPlayer.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/slick/css").Include(
+           "~/assets/css/vendor/slick.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/app").Include(
+            "~/assets/css/app.css", new CssRewriteUrlTransform()));
+
+            // JavaScript Bundles
+            bundles.Add(new ScriptBundle("~/bundles/vendor/js").Include(
+                "~/assets/js/vendor/jquery-3.6.3.min.js",
+                "~/assets/js/vendor/bootstrap.min.js",
+                "~/assets/js/vendor/slick.min.js",
+                "~/assets/js/vendor/jquery-appear.js",
+                "~/assets/js/vendor/jquery-validator.js",
+                "~/assets/js/vendor/aksVideoPlayer.js",
+                "~/assets/js/app.js"));
+            bundles.Add(new ScriptBundle("~/bundles/vendor/min").Include(
+                "~/assets/js/vendor.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app/js").Include(
+                "~/assets/js/app.min.js"));
+
+            // Включение оптимизации
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using AutoMapper;
+using WebApplication.App_Start;
 using WebApplication.BL;
 using WebApplication.BL.Core;
 
@@ -19,6 +21,7 @@ namespace WebApplication
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             // Инициализация AutoMapper
             Mapper.Initialize(cfg =>
