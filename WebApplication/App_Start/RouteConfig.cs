61c00073f3a5a4711833a994f704f843abc48a76
EvaIablocova <125370412+EvaIablocova@users.noreply.github.com>
2024-06-03 17:09:47 +0300

    1

--- WebApplication/App_Start/RouteConfig.cs
+++ WebApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "SignUp", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "LogIn", id = UrlParameter.Optional }
             );
         }
     }
